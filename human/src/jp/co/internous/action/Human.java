889bdafd7df7d7c6f87c12243dc6c7de469f224d
D1097 <itoito761@gmail.com>
2019-05-22 20:50:23 +0900

    5/22fin

--- human/src/jp/co/internous/action/Human.java
+++ human/src/jp/co/internous/action/Human.java
@@ -6,5 +6,11 @@ public class Human {
 
 		HumanName abc=new HumanName();
 		System.out.println(abc.getName());
+
+		Humanage efg=new Humanage();
+		System.out.println(efg.getAge());
+
+		HumanAddress hij=new HumanAddress();
+		System.out.println(hij.getAddress());
 	}
 }

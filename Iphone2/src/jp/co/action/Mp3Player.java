a660dc5de82c6964841fcbeb89745d0fde31a873
D1097 <itoito761@gmail.com>
2019-05-29 19:30:13 +0900

    5/29

---
+++ Iphone2/src/jp/co/action/Mp3Player.java
@@ -0,0 +1,10 @@
+package jp.co.action;
+
+public interface Mp3Player {
+//インターフェイス ルールをプログラミング
+	public abstract void play();
+	public abstract void stop();
+	public abstract void next();
+	public abstract void back();
+
+}
